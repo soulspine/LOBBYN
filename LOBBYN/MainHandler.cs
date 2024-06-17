6bc8cc04a8ec9108e966869934fda275a0e660d0
Olimpia <soulspime@gmail.com>
2024-06-17 18:12:54 +0200

    Add project files.

---
+++ LOBBYN/MainHandler.cs
@@ -0,0 +1,30 @@
+﻿using PoniLCU;
+using static PoniLCU.LeagueClient;
+
+using Salaros.Configuration;
+
+namespace LOBBYN
+{
+    internal class MainHandler
+    {
+        private static LeagueClient _lcu = new LeagueClient(credentials.cmd);
+        private ConfigParser _config = new ConfigParser(File.ReadAllText("config.ini"));
+
+        private string _logfilePath;
+        public MainHandler()
+        {
+            _logfilePath = _config.GetValue("Logger", "logfile");
+        }
+
+        public void log(string message, bool toFile = true)
+        {
+            string dateSig = $"{DateTime.Now.ToString("[dd-MM-yyyy | HH:mm:ss]")}";
+            message = $"{dateSig} {message}\n";
+            Console.WriteLine(message);
+
+            StreamWriter sw = new StreamWriter(_logfilePath);
+            sw.Write(message);
+            sw.Close();
+        }
+    }
+}
---
+++ LOBBYN/Program.cs
@@ -0,0 +1,13 @@
+﻿using PoniLCU;
+
+namespace LOBBYN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainHandler mainHandler = new MainHandler();
+            mainHandler.log("Started LOBBYN.");
+        }
+    }
+}
